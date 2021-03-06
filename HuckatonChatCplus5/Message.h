51a559131cff3ac4c2e76d04f803c5aab482d824
Sergey <eternitydeath@gmail.com>
2021-03-07 02:23:05 +0300

    добавил message.cpp с реализацией методов, в заголовочный файл добавил статическую переменную num для уникальности id.

--- HuckatonChatCplus5/Message.h
+++ HuckatonChatCplus5/Message.h
@@ -3,16 +3,22 @@
 
 class Message
 {
+	static int num;		//уникальный номер, который получает id
 	static int messageCounter;
 	int id;
 	int writerID;
 	int targetID;
 	std::string text;
 public:
+	Message();
 	Message(int writer, std::string text);//сообщение в чат
 	Message(int writer, int target, std::string text);//личное сообщение
+
 	bool searchByTarget(int);
 	bool searchByWriter(int);
 	int getWriter();
+	int getTarget();
+	int getID();
 	std::string getText();
+
 };
