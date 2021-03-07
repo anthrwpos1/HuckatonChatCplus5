d97c35023773fff60107d1bcb21f1bb6ebf91c6a
Vladimir-V <anthrwpos@mail.ru>
2021-03-07 22:28:42 +0300

    MVP всё более-менее работает.

--- HuckatonChatCplus5/Message.h
+++ HuckatonChatCplus5/Message.h
@@ -5,18 +5,17 @@ class Message
 {
 	static int num;		//уникальный номер, который получает id
 	static int messageCounter;
-	int id;
-	int writerID;
-	int targetID;
-	std::string text;
+	int _id;
+	std::string _writer;
+	int _targetID;
+	std::string _text;
 public:
 	Message();
-	Message(int writer, std::string text);//сообщение в чат
-	Message(int writer, int target, std::string text);//личное сообщение
+	Message(std::string writer, std::string text);//сообщение в чат
+	Message(std::string writer, int target, std::string text);//личное сообщение
 
 	bool searchByTarget(int);
-	bool searchByWriter(int);
-	int getWriter();
+	std::string getWriter();
 	int getTarget();
 	int getID();
 	std::string getText();
